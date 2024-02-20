f09b2228cfbaac23d44ee3cada12b53800294d91
Roman <roma.makhunov@gmail.com>
2024-02-20 22:03:30 +0300

    added JWT

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from accounts.api import views
 
 app_name = 'api'
 
 urlpatterns = [
     path('users/', views.ListUserApiView.as_view(), name='api-list-users'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- events_app/settings.py
+++ events_app/settings.py
@@ -106,6 +106,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
 
 # Internationalization
