a52a60cbbec2b5e6d8252c0fb79e2b688eb1f181
Roman <roma.makhunov@gmail.com>
2024-02-24 22:23:15 +0300

    Добавил debug_toolbar

--- events_app/urls.py
+++ events_app/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.api.urls')),
     path('api/', include('events.api.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
