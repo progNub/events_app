689afbc873241a056080b058faf25d03dc083c07
Roman <roma.makhunov@gmail.com>
2024-02-24 20:32:59 +0300

    изменил ListUserApiView

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,27 +1,25 @@
 from django.contrib.auth.hashers import make_password
-from rest_framework import serializers
-from rest_framework.exceptions import ValidationError, PermissionDenied
-from rest_framework.generics import ListAPIView, CreateAPIView, ListCreateAPIView
-from rest_framework.permissions import IsAdminUser, Http404
-from rest_framework.permissions import SAFE_METHODS
+
+from rest_framework.exceptions import PermissionDenied, MethodNotAllowed
+from rest_framework.generics import ListCreateAPIView
 
 from accounts.api.serializers import UserListSerializer, UserCreateSerializer
 from accounts.models import User
 
 
 class ListUserApiView(ListCreateAPIView):
-    queryset = User.objects.all()  # как и где брать объекты
+    queryset = User.objects.all()
     lookup_field = 'id'
-
-    # permission_classes = [IsAdminUser]
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return UserCreateSerializer
-        elif self.request.user.is_superuser:
-            return UserListSerializer
+        elif self.request.method == 'GET':
+            if self.request.user.is_superuser:
+                return UserListSerializer
+            raise PermissionDenied("Нет доступа")
         else:
-            raise PermissionDenied("У вас нет доступа")
+            raise MethodNotAllowed(f"Метод {self.request.method} не поддерживается")
 
     def perform_create(self, serializer):
         data = serializer.validated_data
