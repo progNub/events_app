f40948da972a3919ea79addacc6141d9528aaac6
Roman <roma.makhunov@gmail.com>
2024-02-28 11:33:20 +0300

    linux gunicorn, добавил изменения в файл wsgi

--- events_app/wsgi.py
+++ events_app/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'events_app.settings')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
+
+settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'events_app.settings')
 
 application = get_wsgi_application()
