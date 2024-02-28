3f06621fa0f3f2e813b933cf9ba8517802952804
Roman <roma.makhunov@gmail.com>
2024-02-28 10:34:48 +0300

    linux gunicorn, add database postgres

--- events_app/settings.py
+++ events_app/settings.py
@@ -83,10 +83,20 @@ WSGI_APPLICATION = 'events_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        "USER": os.environ.get('DATABASE_USER'),
+        "PASSWORD": os.environ.get('DATABASE_PASSWORD'),
+        "HOST": os.environ.get('DATABASE_HOST'),
+        "PORT": 5432,
     }
 }
 
